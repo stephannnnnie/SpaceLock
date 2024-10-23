17a12d2d0552019ac59406ff3070e4a08dd4b8ff
yenzxz <yenchihc@usc.edu>
2024-10-23 04:39:08 -0700

    Update GrappleWall.cs

--- SpaceLock/Assets/Scripts/GrappleWall.cs
+++ SpaceLock/Assets/Scripts/GrappleWall.cs
@@ -6,11 +6,35 @@ public class GrappleWall : MonoBehaviour {
     public Material outRangematerial;
    
     void Update() {
-        float distance = Vector3.Distance(player.transform.position, this.transform.position);
+        /**float distance = Vector3.Distance(player.transform.position, this.transform.position);
         if (distance <= player.GetComponent<Grapple>().maxGrappleDistance) {
             this.GetComponent<Renderer>().material = inRangematerial;
         } else {
 
+            this.GetComponent<Renderer>().material = outRangematerial;
+        }
+    }
+    */
+        Grapple grappleScript = player.GetComponent<Grapple>();
+        if (grappleScript == null) return;
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        // Check if the raycast hits this object (the wall)
+        if (Physics.Raycast(ray, out hit, grappleScript.maxGrappleDistance))
+        {
+            if (hit.collider.gameObject == this.gameObject)
+            {
+                this.GetComponent<Renderer>().material = inRangematerial;
+            }
+            else
+            {
+                this.GetComponent<Renderer>().material = outRangematerial;
+            }
+        }
+        else
+        {
             this.GetComponent<Renderer>().material = outRangematerial;
         }
     }
