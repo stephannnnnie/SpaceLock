7a1ab53a9da2818a4549349d9b539fc7f4a76483
Dhruv Rajvansh <104272327+DHRUV5262@users.noreply.github.com>
2024-11-14 23:51:19 -0800

    fix Tutorial

--- SpaceLock/Assets/GameOverTutorial.cs
+++ SpaceLock/Assets/GameOverTutorial.cs
@@ -7,13 +7,19 @@ public class GameOverTutorial : MonoBehaviour
 {
     public Canvas cv;
     public GameObject Player;
-    public float distance = 10f;
+    public float distanceZ = 0.5f;
+    public float distacneX = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(this.transform.position.z - Player.transform.position.z) < distance) {
+        float zz = Mathf.Abs(this.transform.position.z - Player.transform.position.z);
+        float yy = Mathf.Abs(this.transform.position.x - Player.transform.position.x);
+
+        if (zz < distanceZ && yy < distacneX) {
             cv.PlayerLose(1);
+            
         }
+
     }
 }
