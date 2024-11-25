b9ed5474bcd7101ba3fb175501082e86b39603ad
Dhruv Rajvansh <104272327+DHRUV5262@users.noreply.github.com>
2024-11-24 18:41:53 -0800

    UI and Placement changes in Main Menu
    Fixed bug on main menu and clamp camera rotation for main menu

--- SpaceLock/Assets/Scripts/CameraFollow.cs
+++ SpaceLock/Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -6,7 +7,6 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset = new Vector3(0, 5, -10);
     public float rotationSpeed = 1.0f; // Lower sensitivity for smoother movement
     public float mouseSensitivity = 0.4f; // Additional sensitivity control
-
     private float yaw = -90f; // Initialize to -90 degrees
     private float pitch = 0f;
 
@@ -34,6 +34,10 @@ public class CameraFollow : MonoBehaviour
         pitch -= rotationSpeed * Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         // Clamp pitch to prevent camera flipping
+        if (SceneManager.GetActiveScene().name == "3D MainMenu")
+        {
+            yaw = Mathf.Clamp(yaw, -140f, -45f);
+        }
         pitch = Mathf.Clamp(pitch, -60f, 60f);
 
         // Calculate new rotation
