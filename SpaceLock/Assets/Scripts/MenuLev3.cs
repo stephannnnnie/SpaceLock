fd28bb300b0201f0531321cc574afcf00974fb46
Dengheng Shi <shidengheng@gmail.com>
2024-12-02 00:43:23 -0800

    mainmenu updates

--- SpaceLock/Assets/Scripts/MenuLev3.cs
+++ SpaceLock/Assets/Scripts/MenuLev3.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuTut2 : MonoBehaviour
+public class MenuLev3 : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            AdvancedSceneTransition.Instance.LoadScene("Test_tut2", AdvancedSceneTransition.TransitionType.Fade);
+            AdvancedSceneTransition.Instance.LoadScene("Level 3", AdvancedSceneTransition.TransitionType.Fade);
             //SceneManager.LoadScene("Test_tut2");
         }
     }
