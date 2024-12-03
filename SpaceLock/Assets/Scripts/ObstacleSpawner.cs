773e33b039b5fc2efe8bce0bc63e87b812e325db
Lucisar <roseyisar@gmail.com>
2024-12-02 18:40:35 -0800

    add floating effect to ring prefab; adjusted the orientation for bullets power up in the second half of level 3

--- SpaceLock/Assets/Scripts/ObstacleSpawner.cs
+++ SpaceLock/Assets/Scripts/ObstacleSpawner.cs
@@ -129,13 +129,28 @@ public class ObstacleSpawner : MonoBehaviour
 
     void SpawnPowerUpAboveObstacle(GameObject obstacle)
     {
-        // Position the power-up above the obstacle
         Vector3 powerUpPosition = obstacle.transform.position;
-        powerUpPosition.y += obstacle.transform.localScale.y / 2 + 6f; // Adjust height for power-up
 
-        // Randomly select a power-up prefab
-        GameObject powerUpPrefab = powerUpPrefabs[Random.Range(0, powerUpPrefabs.Length)];
-        GameObject powerUp = Instantiate(powerUpPrefab, powerUpPosition, Quaternion.identity);
-        powerUp.transform.SetParent(obstacle.transform); // Attach power-up to obstacle
+        int randomIndex = Random.Range(0, powerUpPrefabs.Length);
+        GameObject powerUpPrefab = powerUpPrefabs[randomIndex];
+        GameObject powerUp = null;
+
+        switch (randomIndex)
+        {
+            case 0:  // bullet
+                powerUpPosition.y += obstacle.transform.localScale.y / 2 + 0.5f;
+                powerUp = Instantiate(powerUpPrefab, powerUpPosition, Quaternion.identity); // may need to consider for different direction value
+                break;
+
+            case 1:  // ring
+                powerUpPosition.y += obstacle.transform.localScale.y / 2 + 6f; // Adjust height for power-up
+                powerUp = Instantiate(powerUpPrefab, powerUpPosition, Quaternion.identity);
+                break;
+        }
+
+        if (powerUp != null)
+        {
+            powerUp.transform.SetParent(obstacle.transform); // Attach power-up to obstacle
+        }
     }
 }
